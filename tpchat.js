2aed2fb001548ff704fac8a488353dbcaaf70534
Saul <saul@saul-desktop.(none)>
2011-06-06 23:54:38 -0700

    url into href, locale-correct date/timestamps, generally functional now

--- tpchat.js
+++ tpchat.js
@@ -1,3 +1,4 @@
+
 var send_text = "";
 var ready_to_send = true;
 
@@ -37,6 +38,11 @@ function send_accum_text()
                      send_text = "";
                   } 
                   ready_to_send = true; 
+                },
+                error: function (xhr, status, err) {
+                    error("remote", status);
+                    clearInterval(sendtimer);
+                    sendtimer = setInterval('send_accum_text()', 2000);
                 }
               })
    }
@@ -56,42 +62,71 @@ function on_load()
        }
     })
 
-    setTimeout('wait_for_chat(0)', 10)
-    setInterval('send_accum_text()', 100)
+    recvtimeout = setTimeout('wait_for_chat(0)', 10)
+    sendtimer = setInterval('send_accum_text()', 200)
 }
 
 var lastt = 0;
 var lasturl = "";
+var tClientStarted = new Date();
+var localOffset = tClientStarted.getTimezoneOffset() * 60000;
+
+function getClockString(d)
+{
+    var h = d.getHours();
+    var m = d.getMinutes();
+    if (h < 10) h = "0" + h;
+    if (m < 10) m = "0" + m;
+    return h + ":" + m;
+}
+
+function getDateString(d)
+{
+    return d.toLocaleDateString();
+}
 
 function post_new_chat(x)
 {
-    $("#log").append($(x).html())
+    var newchat = $(x);
+
+    $(".time", newchat).each(function (i, v) {
+        var d = new Date($(v).text() * 1000 + localOffset);
+        var t = getClockString(d);
+        $(v).text(t);
+    });
+
+    $(".date", newchat).each(function (i, v) {
+        var d = new Date($(v).text() * 1000 + localOffset);
+        var t = getDateString(d);
+        $(v).text(t);
+    });
+
+    $("#log").append(newchat.html());
 
     // scroll to bottom of log
-    $("#log").scrollTop(999999)
+    $("#log").scrollTop(999999);
 
     // for IE (does this assume 12px font height?)
-    var h1 = $("#log").scrollTop()
-    $("#log").scrollTop(h1*12)
+    var h1 = $("#log").scrollTop();
+    $("#log").scrollTop(h1*12);
+
 }
 
 function wait_for_chat(t)
 {
-    var d = new Date()
-    var gmtHours = -d.getTimezoneOffset()/60 
     lastt = t;
-    lasturl = "/log?tz=" + gmtHours + "&t=" + t
-    $.get(lasturl, function(x) {
+    lasturl = "/log?t=" + t
+    $.get(lasturl).success(function(x) {
           post_new_chat(x);
           var t1 = $(x).attr("t");
           if (!isFinite(t1)) t1 = t;
        	  wait_for_chat(t1);
 
           return true;
+    }).error(function (x) {
+        error("remote", lasturl + " failed")
     });
 }
 
-
-$(document).ready(on_load)
-
+$(document).ready(on_load);
 
--- tpchat.py
+++ tpchat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from twisted.internet import reactor, protocol
+from twisted.internet import reactor, protocol, defer
 from twisted.python import log
 from twisted.python.components import registerAdapter
 from twisted.web.server import Site, NOT_DONE_YET, Session
@@ -11,12 +11,39 @@ from twisted.web.util import Redirect
 import twisted.protocols.basic
 from zope.interface import Interface, Attribute, implements
 
+import time
+import os
+import re
+
 ircd_passwd = "blah"
 ircd_servername = "ircweb-dev2.localhost"
 ircd_serverdesc = "Textpunks dev server"
 
+divTimestamp = '<div class="msg timestamp">'
+fmtdivTimestamp = divTimestamp + '<div class="date">%s</div></div>'
+
+fmtdivChatline = '''
+<div class="msg">
+<div class="time">%s</div>
+<div class="src"> [%s]</div>
+<div class="contents"> %s</div>
+</div>''' 
+
+regexUrl = re.compile(r"(http://\S+)", re.IGNORECASE | re.LOCALE)
+
 def getClockString():
-    return "00:00"
+    t = time.localtime()
+
+    if t.tm_isdst == 0:
+        tz = time.timezone
+    else:
+        tz = time.altzone
+
+    return int(time.time() - tz)
+
+def getDateString():
+    return getClockString()
+#    return time.strftime("%Y %B %d %Z", time.gmtime())
 
 class IUser(Interface):
     value = Attribute("user session")
@@ -28,8 +55,12 @@ class User(object):
 
 registerAdapter(User, Session, IUser)
 
-def isValidNick(s):
-    return len(s) > 0
+def isValidNick(nick):
+    if len(nick) == 0:
+        return False
+    if nick in root.ircd.names:
+        return False
+    return True
 
 class Channel(Resource):
     isLeaf = True
@@ -37,86 +68,148 @@ class Channel(Resource):
         self.name = name
         self.logfn = self.name + ".log"
         self.listeners = { }
-        self.contents = "--- channel %s\n" % name
-        self.joinedIRC = False
+        self.lastWriteTime = 0
+        try:
+            self.contents = file(self.logfn).read()
+        except IOError:
+            self.contents = ""
 
-    def logwrite(self, msg):
-        self.contents += msg
+        root.ircd.join(name)
+
+    def __str__(self):
+        return "[channel %s]" % self.name
+
+    def debug(self):
+        return "%s: %s listeners, %s bytes of content\n" % (self.name, len(self.listeners), len(self.contents))
+
+    def logwrite(self, src, msg, fromIRC=False):
+        xmlmsg = str(msg)
+        xmlmsg.replace("\n", "<br/>")
+        matchobj = regexUrl.search(xmlmsg)
+        if matchobj is not None:
+            url = matchobj.groups()[0]
+            htmlLink = '<a href="%s" target="_blank">%s</a>' % (url, url)
+            xmlmsg = xmlmsg[0:matchobj.start()] + htmlLink + xmlmsg[matchobj.end():]
+
+        data = ""
+
+        lastWriteTime = self.lastWriteTime
+        self.lastWriteTime = time.time()
+
+        lastymd = time.gmtime(lastWriteTime)[0:2]
+        nowymd = time.gmtime()[0:2]
+
+        if lastymd != nowymd:
+            data += fmtdivTimestamp % getDateString()
+
+        data += fmtdivChatline % (getClockString(), src or self.name, xmlmsg)
+
+        self.contents += data
 
         with file(self.logfn, "a") as fp:
-            fp.write(msg)
+            fp.write(data)
+
+        if not fromIRC:
+            root.ircd.sendToChannel(self.name, src, msg)
 
         for nick, req in self.listeners.iteritems():
-            req.write(msg)
+            smsg = '<span t="%s" id="log">%s</span>' % (len(self.contents), data)
+            req.write(smsg)
             req.finish()
 
         self.listeners = { }
 
     def render_GET(self, req):
-        t = req.args.get("t", 0)
-        if t > len(self.contents):
+        if not req.user.nick:
+            req.setResponseCode(404)
+            return NOT_DONE_YET # XXX: leak. cause client js to error instead
+
+        if "t" in req.args:
+            t = int(req.args["t"][0])
+        else:
+            t = self.contents.rfind(divTimestamp)
+
+        if t >= len(self.contents):
             self.listeners[req.user.nick] = req
             return NOT_DONE_YET
 
         history = self.contents[t:]
-
         history.replace("\n", "<br/>")
-      
-        msg = '<span t="%s" id="log">%s</span>' % (len(self.contents), history)
-        print msg
+        msg = '<span t="%s" id="log">%s</span>' % (len(self.contents)+1, history)
         return msg
 
     def render_POST(self, req):
-        msg = "%s [%s] %s" % (getClockString(), req.user.nick, req.args["chatline"][0])
-        self.logwrite(msg)
+        if not req.user.nick:
+            req.setResponseCode(404)
+            return "not logged in"
+
+        self.logwrite(req.user.nick, req.args["chatline"][0])
         return "OK"
 
-LoginPage = File("login.html")
-MainPage = File("chat.html")
+class DumpInfo(Resource):
+    isLeaf = True
 
-def getChannel(req):
-    return req.getHeader("host").split(".")[-3]
+    def render_GET(self, req):
+        req.setHeader("Content-Type", "text/plain")
+        ret = root.ircd.debug() + "\n"
+
+        for chname, ch in root.channels.items():
+            ret += "%s" % ch.debug()
+    
+        return ret
 
 class tpchat(Resource):
-#    isLeaf = True
     def __init__(self):
         Resource.__init__(self)
         self.channels = { }
+        self.ircd = None
 
     def getChild(self, path, req):
-        channame = getChannel(req)
+        hostparts = req.getHeader("host").split(".")
 
-        if channame not in self.channels:
-            self.channels[channame] = Channel(channame)
+        if len(hostparts) < 3:
+            channame = "errors"
+        else: 
+            channame = hostparts[-3]
 
-        channel = self.channels[channame]
-        print channame, path, req, channel
+        channel = self.getChannel("#" + channame)
 
         req.user = IUser(req.getSession())
+
+        print req, channel, path, req.user.nick
+
+        if path in staticFiles:
+            return staticFiles[path]
 
         if path == "login":
             if "nick" in req.args and isValidNick(req.args["nick"][0]):
                 n = req.args["nick"][0]
 
                 req.user.nick = n
-                return MainPage
+                return Redirect("/")
 
-            return LoginPage
-
-        if path == "logout":
-            req.getSession().expire()
-            return LoginPage
-
-        if not req.user.nick:
             return LoginPage
 
         if path == "log":
             return channel
 
+        if not req.user.nick:
+            return Redirect("/login")
+
+        if path == "logout":
+            req.getSession().expire()
+            return LoginPage
+
         if not path:
-            return MainPage
+            return FileTemplate("chat.html", { 'nickname': req.user.nick })
 
         return Resource.getChild(self, path, req)
+    
+    def getChannel(self, channame):
+        if channame not in self.channels:
+            self.channels[channame] = Channel(channame)
+
+        return self.channels[channame]
 
 class tpircd(twisted.protocols.basic.LineReceiver):
     def __init__(self):
@@ -124,12 +217,14 @@ class tpircd(twisted.protocols.basic.LineReceiver):
         self.names = { }
         self.nextuid = 0xAAAAA
         self.sid = "100B"
-        self.contents = { }
 
-    def log(self, cname, data):
-        logline = data.strip() + "\n"
-        channel = root.getChannel(cname)
-        channel.logwrite(logline)
+    def debug(self):
+        ret = "sid=%s\n" % self.sid
+
+        for k, id in self.names.items():
+            ret += "%s %s\n" % (k, id)
+
+        return ret
 
     def lineReceived(self, line):
         if not line:
@@ -143,21 +238,37 @@ class tpircd(twisted.protocols.basic.LineReceiver):
 
         cmd, _, rest = line.partition(" ")
 
-        try:
-            r = tpircd.__dict__["on_" + cmd](self, src, rest)
-            if r:
-                print "<", line
-        except KeyError:
-            print "??", src, line
+        handler = "on_" + cmd
+        if handler in tpircd.__dict__:
+            r = True
+            try:
+                r = tpircd.__dict__[handler](self, src, rest)
+            finally:
+                if r:
+                    print "<", line
+        else:
+            print "UNHANDLED %s :%s %s" % (cmd, src, line)
 
     def connectionMade(self):
         self.send("PASS %s 02110000 |" % ircd_passwd)
         self.send("SERVER %s 1 %s :%s" % (ircd_servername, self.sid, ircd_serverdesc))
         self.loguid = self.getuid("_", 0xFFFFF)
 
+        root.ircd = self
+
+        reactor.listenTCP(4444, factory)
+
     def send(self, line):
-        print ">", line
+#        print ">", line
         self.transport.write(line + "\n")
+
+    def sendToChannel(self, chan, src, msg):
+        msg = str(msg)
+        for i in xrange(0, len(msg), 450):
+            self.send(":%s PRIVMSG %s :%s" % (self.getuid(src), chan, msg[i:i+450]))
+
+    def join(self, cname):
+        self.send(":%s NJOIN %s :%s" % (self.sid, cname, self.loguid))
 
     def getuid(self, name, newuid=None):
         if not name:
@@ -175,6 +286,8 @@ class tpircd(twisted.protocols.basic.LineReceiver):
 
             self.send(":%s UNICK %s %s %s localhost 127.0.0.1 +i :%s" % (
                        self.sid, name, uid, name, name))
+        else:
+            uid = self.uids[name]
 
         return uid
 
@@ -188,9 +301,7 @@ class tpircd(twisted.protocols.basic.LineReceiver):
         if src in self.names:
             src = self.names[src]
 
-        logline = "%s [%s] %s" % (getClockString(), src, rest[1:])
-
-        self.log(channel, logline) # XXX
+        root.getChannel(channel).logwrite(src, rest[1:], fromIRC=True)
 
         return False
 
@@ -214,22 +325,31 @@ class tpircd(twisted.protocols.basic.LineReceiver):
         cname, rest = rest.split(" ", 1)
         channel = root.getChannel(cname)
 
-        if not channel.joinedIRC:
-            self.send(":%s NJOIN %s :%s" % (self.sid, cname, self.loguid))
-            channel.joinedIRC = True
-
         return True
 
 class ircdFactory(protocol.ReconnectingClientFactory):
     protocol = tpircd
 
+class FileTemplate(Resource):
+    def __init__(self, fn, d):
+        self.contents = file(fn, "r").read() % d
+
+    def render_GET(self, req):
+        return self.contents
+
+LoginPage = File("login.html")
+
+staticFiles = {
+    'robots.txt': File("robots.txt"),
+    'favicon.ico': File("favicon.ico"),
+    'style.css': File("style.css"),
+    'tpchat.js': File("tpchat.js"),
+    'debug': DumpInfo()
+}
+
 root = tpchat()
 
-root.putChild("robots.txt", File("robots.txt"))
-root.putChild("favicon.ico", File("favicon.ico"))
-
 factory = Site(root)
-reactor.listenTCP(4444, factory)
 
 # https://<channel>.ideatrial.com -> if not nick, redirect to /login.html
 reactor.connectTCP("localhost", 6667, ircdFactory())
